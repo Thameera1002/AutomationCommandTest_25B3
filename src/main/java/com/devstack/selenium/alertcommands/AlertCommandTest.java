115bd4461ca65d6ac8cd6bcbd4b55bd9846cd207
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2026-01-07 08:15:05 +0530

    AlertCommandTest.java updated

--- src/main/java/com/devstack/selenium/alertcommands/AlertCommandTest.java
+++ src/main/java/com/devstack/selenium/alertcommands/AlertCommandTest.java
@@ -8,7 +8,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AlertCommandTest {
     public static void main(String[] args) throws InterruptedException {
-       WebDriver driver = WebDriverManager.chromedriver().create();
+        //alertWithOK();
+        alertWithOKAndCancel();
+    }
+
+    public static void alertWithOK() throws InterruptedException{
+               WebDriver driver = WebDriverManager.chromedriver().create();
        driver.manage().window().maximize();
        driver.get("https://demo.automationtesting.in/Alerts.html");
        driver.findElement(By.xpath("//a[normalize-space(text())='Alert with OK']")).click();
@@ -18,5 +23,27 @@ public class AlertCommandTest {
        System.out.println("Alert Text: " + alert.getText());
        Thread.sleep(3000);
          alert.accept();
+    }
+
+        public static void alertWithOKAndCancel() throws InterruptedException{
+               WebDriver driver = WebDriverManager.chromedriver().create();
+       driver.manage().window().maximize();
+       driver.get("https://demo.automationtesting.in/Alerts.html");
+       driver.findElement(By.xpath("//a[normalize-space(text())='Alert with OK & Cancel']")).click();
+       driver.findElement(By.cssSelector("[class='btn btn-primary']")).click();
+       Thread.sleep(3000);
+       Alert alertAccept = driver.switchTo().alert();
+       System.out.println("Alert Text: " + alertAccept.getText());
+       Thread.sleep(3000);
+         alertAccept.accept();
+        System.out.println(driver.findElement(By.id("demo")).getText());
+
+        driver.findElement(By.cssSelector("[class='btn btn-primary']")).click();
+       Thread.sleep(3000);
+       Alert alertCancel = driver.switchTo().alert();
+       System.out.println("Alert Text: " + alertCancel.getText());
+       Thread.sleep(3000);
+         alertCancel.dismiss();
+        System.out.println(driver.findElement(By.id("demo")).getText());
     }
 }
