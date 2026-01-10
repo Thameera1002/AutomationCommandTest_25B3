cb075582baa23c89f5ef140f46f043a8133eef4b
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2026-01-10 12:10:57 +0530

    WindowCommandTest.java updated

--- src/main/java/com/devstack/selenium/windowcommands/WindowCommandTest.java
+++ src/main/java/com/devstack/selenium/windowcommands/WindowCommandTest.java
@@ -10,7 +10,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class WindowCommandTest {
     public static void main(String[] args) throws InterruptedException {
-       WebDriver driver = WebDriverManager.chromedriver().create();
+        // openNewTabbedWindow();
+        openNewSeparateWindow();
+    }
+
+    public static void openNewTabbedWindow() throws InterruptedException{
+             WebDriver driver = WebDriverManager.chromedriver().create();
        driver.manage().window().maximize();
        driver.get("https://demo.automationtesting.in/Windows.html");
        Thread.sleep(2000);
@@ -39,5 +44,38 @@ public class WindowCommandTest {
             System.out.println("Parent window title: " + parentTitle);
             Thread.sleep(2000);
             Thread.sleep(2000);
+    }
+
+    public static void openNewSeparateWindow() throws InterruptedException{
+         WebDriver driver = WebDriverManager.chromedriver().create();
+       driver.manage().window().maximize();
+       driver.get("https://demo.automationtesting.in/Windows.html");
+       Thread.sleep(2000);
+       driver.findElement(By.xpath("//a[text()='Open New Seperate Windows']")).click();
+       Thread.sleep(2000);
+       String parentWindow = driver.getWindowHandle();
+       Thread.sleep(2000);
+       driver.findElement(By.cssSelector("[class='btn btn-primary']")).click();
+       Thread.sleep(2000);
+       Set<String> getAllWindows = driver.getWindowHandles();
+         for (String window : getAllWindows) {
+            if(!window.equals(parentWindow)){
+                driver.switchTo().window(window);
+                Thread.sleep(2000);
+                @Nullable
+                String title = driver.getTitle();
+                System.out.println("Child window title: " + title);
+                Thread.sleep(2000);
+                driver.close();
+                break;
+            }
+        }
+            driver.switchTo().window(parentWindow);
+                Thread.sleep(2000);
+                @Nullable
+                String parentTitle = driver.getTitle();
+                System.out.println("Parent window title: " + parentTitle);
+                Thread.sleep(2000);
+                Thread.sleep(2000);
     }
 }
