c2a6c00f3989f37cc394853c193a2957afbd8768
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2026-01-13 18:55:53 +0530

    WaitCommandTest.java updated.

--- src/main/java/com/devstack/selenium/waitcommands/WaitCommandTest.java
+++ src/main/java/com/devstack/selenium/waitcommands/WaitCommandTest.java
@@ -1,11 +1,13 @@
 package com.devstack.selenium.waitcommands;
 
 import java.time.Duration;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,7 +15,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class WaitCommandTest {
     public static void main(String[] args) {
         // implicitWaitExample();
-        explicitWaitExample();
+        // explicitWaitExample();
+        fluentWaitExample();
     }
 
     public static void implicitWaitExample() {
@@ -31,6 +34,19 @@ public class WaitCommandTest {
         driver.get("https://demoqa.com/dynamic-properties");
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.id("enableAfter")));
+        element.click();
+        System.out.println("Clicked on the button successfully");
+    }
+
+    public static void fluentWaitExample() {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/dynamic-properties");
+        FluentWait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+                .withTimeout(Duration.ofSeconds(10))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class);
+        WebElement element = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("visibleAfter")));
         element.click();
         System.out.println("Clicked on the button successfully");
     }
