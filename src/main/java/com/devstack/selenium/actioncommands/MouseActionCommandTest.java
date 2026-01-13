1e7f4ca2519cf3a92b91d72614d1fb9cd6d4081e
Thameera Malinga <33679537+Thameera1002@users.noreply.github.com>
2026-01-13 19:28:24 +0530

    MouseActionCommandTest.java updated

--- src/main/java/com/devstack/selenium/actioncommands/MouseActionCommandTest.java
+++ src/main/java/com/devstack/selenium/actioncommands/MouseActionCommandTest.java
@@ -1,18 +1,17 @@
 package com.devstack.selenium.actioncommands;
 
-import javax.swing.Action;
-
+import org.jspecify.annotations.Nullable;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MouseActionCommandTest {
     public static void main(String[] args) throws InterruptedException {
-        contextClickExample();
+        // contextClickExample();
+        doubleClickExample();
     }
     
     public static void contextClickExample() throws InterruptedException {
@@ -27,5 +26,18 @@ public class MouseActionCommandTest {
         String alertText = alert.getText();
         System.out.println("Alert text: " + alertText);
         alert.accept();
+    }
+
+    public static void doubleClickExample() throws InterruptedException {
+        WebDriver driver = WebDriverManager.chromedriver().create();
+        driver.manage().window().maximize();
+       driver.get("https://unixpapa.com/js/testmouse.html");
+       WebElement clickableTestLink = driver.findElement(By.linkText("click here to test"));
+         Actions action = new Actions(driver);
+         action.doubleClick(clickableTestLink).perform();
+         Thread.sleep(3000);
+         @Nullable
+        String domTextValue = driver.findElement(By.cssSelector("textarea")).getAttribute("value");
+        System.out.println("DOM Text Area Value: " + domTextValue);
     }
 }
